2d8860e3a1137cf9c3170236c711e50643a3455d
KalininMN <ar1esmax1mwork@gmail.com>
2022-05-30 12:31:29 +0400

    Added OnlineBooking

--- src/API/OnlineBookingAPI.js
+++ src/API/OnlineBookingAPI.js
@@ -1,7 +1,66 @@
 const router = require(`${rootPath}/plugins/router.js`);
+const db = require(`${rootPath}/utils/dbConnect`);
 
 router.get('/onlineBooking', (req, res) => {
   res.sendFile(`${rootPath}/pages/OnlineBookingPage.html`);
+});
+
+router.post('/v2/onlineBooking', async (req, res) => {
+  console.log(req.body);
+  const {
+    organizationName,
+    contactPerson,
+    city,
+    mail,
+    place,
+    time,
+    workingMode,
+    productType,
+    numbersOfTitles,
+    storageCapacity,
+    numbersOfCars,
+    averageQuantity,
+    maxBoxWeight,
+    numbersOfBox,
+    orderStructure,
+    additionalServices,
+  } = req.body;
+
+  await new Promise((resolve, reject) => {
+    db.execute(
+      'INSERT INTO online_booking (organizationName, contactPerson, '
+      + 'city, mail, place, time, workingMode, productType, numbersOfTitles, '
+      + 'storageCapacity, numbersOfCars, averageQuantity, maxBoxWeight, '
+      + 'numbersOfBox, orderStructure, additionalServices) VALUES (?, ?, ?, ?, ?, '
+      + '?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);',
+      [
+        organizationName,
+        contactPerson,
+        city,
+        mail,
+        place,
+        time,
+        workingMode,
+        productType,
+        numbersOfTitles,
+        storageCapacity,
+        numbersOfCars,
+        averageQuantity,
+        maxBoxWeight,
+        numbersOfBox,
+        orderStructure,
+        additionalServices,
+      ],
+      (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+      }
+    );
+  });
+
+  res.sendStatus(200);
 });
 
 module.exports = router;
